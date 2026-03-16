f7bc042aaa4eb213dd0536d9a76592e99fe5ef4f
Cristobal Andrade <cristobal.andradeq5@gmail.com>
2026-03-16 16:18:38 -0300

    Agregada rutas y controllers

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const User = require('../models/user')
+
+exports.getUsers = async (req, res) => {
+
+ const users = await User.findAll()
+
+ res.json(users)
+
+}
+
+exports.createUser = async (req, res) => {
+
+ try {
+
+  const user = await User.create(req.body)
+
+  res.status(201).json(user)
+
+ } catch (error) {
+
+  res.status(500).json({ error: error.message })
+
+ }
+
+}
--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+const sequelize = require('./config/database')
+
 const mainRoutes = require('./routes/mainRoutes')
+
+const userRoutes = require('./routes/userRoutes')
+
+app.use(express.json())
+app.use('/api', userRoutes)
 
 const logger = require('./middlewares/logger')
 
@@ -16,6 +23,12 @@ app.use(logger)
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', mainRoutes)
+
+sequelize.sync()
+ .then(() => {
+  console.log("Base de datos conectada")
+ })
+ .catch(err => console.log(err))
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en puerto ${PORT}`)
